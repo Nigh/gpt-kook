265a36cdff2b4f233fa3c0a202dd64fcdee76540
xianii <xianyi.xia@outlook.com>
2024-01-08 20:51:35 +0800

    use kook compatible emoji

--- src/main.go
+++ src/main.go
@@ -277,12 +277,12 @@ func commonChanHandler(ctx *kook.KmarkdownMessageContext) {
 		defer func() {
 			delete(busyChannel, ctxCommon.TargetID)
 		}()
-		ctx.Session.MessageAddReaction(ctxCommon.MsgID, "⌛️")
+		ctx.Session.MessageAddReaction(ctxCommon.MsgID, "🔸️")
 		ans := openaiezgo.NewSpeech(ctxCommon.TargetID, words)
 		if len(ans) > 0 {
 			reply(ans)
 		}
-		ctx.Session.MessageDeleteReaction(ctxCommon.MsgID, "⌛️", botID)
+		ctx.Session.MessageDeleteReaction(ctxCommon.MsgID, "🔸️", botID)
 		ctx.Session.MessageAddReaction(ctxCommon.MsgID, "✅")
 	}
 
